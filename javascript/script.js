e69ae8b1e153c8229a944aa4c293344ea199c8ac
Everton Ferreira <ef9793693@gmail.com>
2025-05-04 21:25:26 -0400

    por hj deu

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#mobile-btn').on('click', function () {
-        $('#mobile-menu').toggleclass('active')
+        $('#mobile-menu').toggleClass('active');
+        $('#mobile-btn').find('i').toggleClass('fa-x');
     });
 });
